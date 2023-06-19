adc9fb4b8db51dd9797963e6417a126d367f53cc
Georgi St. Lukanov <goshiii87@gmail.com>
2023-06-19 16:40:15 +0200

    models register in adminpanel

--- Notes_App_remake_exam/my_web/admin.py
+++ Notes_App_remake_exam/my_web/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from Notes_App_remake_exam.my_web.models import Profile, Note
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'age',)
+
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title',)
